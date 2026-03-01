90c702550a9bd21b6c1548d54cf6f15c4d1c6243
andold <andold@naver.com>
2026-03-01 20:21:24 +0900

    try bug fix) 비표준 데이터에 대해 관대하게 처리 - ical4j.properties

---
+++ src/test/java/kr/andold/terran/ics/service/IcsServiceTest.java
@@ -0,0 +1,29 @@
+package kr.andold.terran.ics.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import kr.andold.utils.Utility;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IcsServiceTest {
+	@Autowired private IcsService service;
+
+	@BeforeEach
+	public void before() {
+		log.info(Utility.HR);
+	}
+
+	@Test
+	public void downloadIcs() {
+		assertThat(service);
+		log.info(service.downloadIcs(1028));
+	}
+
+}
